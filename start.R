8cbd4b3812cb9417fe19940c752efd516624ab1a
ihsankahveci <ikahveci@uw.edu>
2022-10-23 17:51:13 -0700

    commiting unsaved changes from last time

---
+++ start.R
@@ -0,0 +1,8 @@
+library(datadrivencv)
+
+datadrivencv::use_datadriven_cv(
+  full_name = "Ihsan Kahveci", 
+  data_location = "https://docs.google.com/spreadsheets/d/1g2dM7OsKADP-RWAR1A7QEi70yPqC9mupyWuhqnNNy2g/edit?usp=sharing",
+  pdf_location = "CV.pdf", 
+  html_location = "CV.html",
+  use_network_logo = FALSE)
